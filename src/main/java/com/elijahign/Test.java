5d7503483d70e2f181dc2987209a5ec0162d5497
ElijahIgn <eignacio0001@mymail.lausd.net>
2025-05-30 10:20:32 -0700

    Update Test.java

--- src/main/java/com/elijahign/Test.java
+++ src/main/java/com/elijahign/Test.java
@@ -1,5 +1,19 @@
-public class Test {
-    public static void main(String[] args) {
-        
-    }
+package com.elijahign;
+
+import com.birdbrain.Finch;
+
+public class Test{
+public static void main(String[] args) {
+    Finch myFinch = new Finch();
+    myFinch.setMotors(-50,50);
+
+    
+
+    myFinch.stopAll();
+    myFinch.disconnect();
+
+
+
+}
+
 }
