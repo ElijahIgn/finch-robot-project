54b99853231eb7b9b16e69a0589059b11b53cbda
ElijahIgn <eignacio0001@mymail.lausd.net>
2025-06-02 10:09:18 -0700

    while loop

--- src/main/java/com/elijahign/Test.java
+++ src/main/java/com/elijahign/Test.java
@@ -6,9 +6,12 @@ public class Test{
 public static void main(String[] args) {
     Finch myFinch = new Finch();
     myFinch.setMotors(-50,50);
+myFinch.setMove("F",15,100);
 
-    
-
+while(myFinch.getDistance() < 10){
+myFinch.setTurn("L",90,50);
+myFinch.setMove("F",100,100);
+}
     myFinch.stopAll();
     myFinch.disconnect();
 
