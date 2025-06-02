3cee6fe3d38707438594063bbd29a124c189f7c9
ElijahIgn <eignacio0001@mymail.lausd.net>
2025-06-02 10:34:58 -0700

    2nd while looop

--- src/main/java/com/elijahign/Test.java
+++ src/main/java/com/elijahign/Test.java
@@ -6,11 +6,15 @@ public class Test{
 public static void main(String[] args) {
     Finch myFinch = new Finch();
     myFinch.setMotors(-50,50);
-myFinch.setMove("F",15,100);
 
-while(myFinch.getDistance() < 10){
-myFinch.setTurn("L",90,50);
-myFinch.setMove("F",100,100);
+
+while(myFinch.getDistance() >=7){
+    myFinch.setMove("F",15,100);
+}
+
+while(myFinch.getDistance() < 7){
+    myFinch.setTurn("L",90,50);
+    myFinch.setMove("F",15,100);
 }
     myFinch.stopAll();
     myFinch.disconnect();
