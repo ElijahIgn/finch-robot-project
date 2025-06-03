5a925a17e698ca3def49c878a656bd5881abb94a
ElijahIgn <eignacio0001@mymail.lausd.net>
2025-06-03 10:20:09 -0700

    Update Test.java

--- src/main/java/com/elijahign/Test.java
+++ src/main/java/com/elijahign/Test.java
@@ -7,15 +7,15 @@ public static void main(String[] args) {
     Finch myFinch = new Finch();
     myFinch.setMotors(-50,50);
 
-
+// while loop that makes the finch move forward
 while(myFinch.getDistance() >=7){
     myFinch.setMove("F",15,100);
-}
-
 while(myFinch.getDistance() < 7){
     myFinch.setTurn("L",90,50);
-    myFinch.setMove("F",15,100);
-}
+    myFinch.setMove("B",7,100);
+}}
+// if there is something infront of the finch it will turn left and move
+
     myFinch.stopAll();
     myFinch.disconnect();
 
