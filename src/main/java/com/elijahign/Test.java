21d50386d24e51d6c13291ad4d0b25e3e5bde04d
ElijahIgn <eignacio0001@mymail.lausd.net>
2025-06-06 09:02:51 -0700

    2nd half

--- src/main/java/com/elijahign/Test.java
+++ src/main/java/com/elijahign/Test.java
@@ -11,12 +11,7 @@ public static void main(String[] args) {
 while(myFinch.getDistance() >=7){
     myFinch.setMove("F",15,100);
 }
-    // 2nd whileloopif there is something infront of the finch it will turn left and move
-// while(myFinch.getDistance() < 7){
-//     myFinch.setMove("B",7,100);
-//     myFinch.setTurn("L",90,50);
-    
-// }
+//these two actions ensure smooth movement for the finch    
 myFinch.setMove("B",7,100);
 myFinch.setTurn("L",90,50);
 
@@ -24,13 +19,19 @@ while(myFinch.getDistance() >= 7){
     myFinch.setMove("F",30,100);
 }
 
-myFinch.setMove("B",7,100);
+myFinch.setMove("B",20,100);
 myFinch.setTurn("L",90,50);
+myFinch.setMove("F",30,100);
+myFinch.setTurn("R",90,50);
+while(myFinch.getDistance() >= 7){
+    myFinch.setMove("F",30,100);
+}
+
+
+
 
     myFinch.stopAll();
     myFinch.disconnect();
-
-
 
 }
 
