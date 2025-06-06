4c68d7fe628d5a1932637cdf4b6e86c79a23ee10
ElijahIgn <eignacio0001@mymail.lausd.net>
2025-06-06 10:25:44 -0700

    renamed class, final updates Test.java -> FinalFinch.java

--- src/main/java/com/elijahign/FinalFinch.java
+++ src/main/java/com/elijahign/FinalFinch.java
@@ -2,14 +2,14 @@ package com.elijahign;
 
 import com.birdbrain.Finch;
 
-public class Test{
+public class FinalFinch{
 public static void main(String[] args) {
     Finch myFinch = new Finch();
     myFinch.setMotors(50,50);
 
 // while loop that makes the finch move forward
 while(myFinch.getDistance() >=7){
-    myFinch.setMove("F",15,100);
+    myFinch.setMove("F",30,100);
 }
 //student-made method 
 myFinch.readjust();
@@ -22,10 +22,24 @@ while(myFinch.getDistance() >= 7){
 myFinch.readjust();
 myFinch.setMove("F",30,100);
 myFinch.setTurn("R",90,50);
-while(myFinch.getDistance() >= 7){
-    myFinch.setMove("F",30,100);
-}
 
+    myFinch.setMove("F",50,50);
+
+myFinch.setTurn("R",90,50);
+
+
+
+    myFinch.setMove("F",110,40);
+
+
+myFinch.setTurn("R",90,50);
+
+    myFinch.setMove("F",40,35);
+
+myFinch.readjust();
+myFinch.setMove("F",30,100);
+myFinch.setTurn("L",90,50);
+myFinch.setMove("F",30,100);
 
 
 
